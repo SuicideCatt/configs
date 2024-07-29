c7e78ea616fd48fe911ec4894574cb911f0e28b9
SuicideCatt <fr7g31@gmail.com>
2024-07-29 14:21:59 +0300

    update log.sh

--- log.sh
+++ log.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/sh
 
-log()        { echo "SCT_CFG: $1"; }
-lstatus()    { log "$1..."; }
+log()        { printf "SCT_CFG: $1"; }
+lstatus()    { log "$1... "; }
 
 llinking()   { lstatus "Linking $1"; }
 linstaling() { lstatus "Instaling $1"; }
 lupdating()  { lstatus "Updating $1"; }
 
-ldone()      { log Done; }
-lerror()     { log Error; }
-lskip()      { log Skiped; }
+ldone()      { printf "Done\n"; }
+lerror()     { printf "Error\n"; }
+lskip()      { printf "Skiped\n"; }
